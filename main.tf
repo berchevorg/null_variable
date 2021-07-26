9a2c595774395d4334b4949ffc4aeed3c4c385be
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2021-07-26 07:51:25 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,14 @@
-variable "name" {}
+variable "name1" {}
+variable "name2" {}
 
-resource "null_resource" "helloWorld" {
+resource "null_resource" "helloWorld1" {
   provisioner "local-exec" {
-    command = "echo Hello Nikolay"
+    command = "echo Hello ${var.name1}"
   }
 }
 
 resource "null_resource" "helloWorld2" {
   provisioner "local-exec" {
-    command = "echo Hello ${var.name}"
+    command = "echo Hello ${var.name2}"
   }
 }
